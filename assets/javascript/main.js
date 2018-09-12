745e21a0c8488396bf52b5a3cc6c2f1a315d8718
Kevin Do <do.kevin.vo@gmail.com>
2018-09-12 09:42:22 -0700

    Replaces moodmusic img

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,8 +2,3 @@
 $(".port-item").click(function() {
     $(".collapse").collapse("hide");
 });
-
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
