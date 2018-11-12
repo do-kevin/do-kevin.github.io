e96420c488bfc116ef0f8ddc283f80378269813e
Kevin Do <do.kevin.vo@gmail.com>
2018-11-11 21:06:35 -0800

    Updates

---
+++ precache-manifest.9d7ed6306ba02e7d7749576398dddd85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "700c8ff9efefceb2af0a",
+    "url": "/static/js/main.700c8ff9.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "700c8ff9efefceb2af0a",
+    "url": "/static/css/main.4c24c8c2.chunk.css"
+  },
+  {
+    "revision": "2f186b6e0243c1a605d68de450d237ae",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a445f648f91cf2b28d3a5f36f7926f0.js"
+  "/precache-manifest.9d7ed6306ba02e7d7749576398dddd85.js"
 );
 
 workbox.clientsClaim();
