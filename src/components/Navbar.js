39a3f061fc46bbb4605735f6f3492851c1e2b1bf
Kevin Do <do.kevin.vo@gmail.com>
2018-11-13 00:00:28 -0800

    Completes sticky navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,52 @@ import React, { Component } from "react";
 import "../css/Navbar.css";
 
 class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            toStickOrNot: null,
+            sticky: {
+                top: "0",
+                width: "100%",
+                position: "fixed",
+                zIndex: "1"
+            },
+            placeholderColor: null
+        };
+        this.handleScroll = this.handleScroll.bind(this)
+    }
+
+    componentDidMount(){
+        window.addEventListener("scroll", this.handleScroll);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+    
+    handleScroll() {
+    
+      if (window.scrollY > 100) {
+        this.setState({
+            toStickOrNot: this.state.sticky,
+            placeholderColor: {background: "#ffc800"}
+        });
+      } else if (window.scrollY < 100) {
+        this.setState({
+            toStickOrNot: null,
+            placeholderColor: {background: "#005fc2"}
+        });
+      }
+    
+    }
+
+
   render() {
+    
+    console.log(window.scrollY, "scrollY");
+    console.log(window.innerHeight, "innerHeight");
+
     return (
         <div>
             <section 
@@ -23,8 +68,9 @@ class Navbar extends Component {
             <nav 
                 className="breadcrumb is-centered" 
                 id="menu-bar"
+                style={this.state.toStickOrNot}
             >
-                <div id="menu-box-placeholder"></div>
+                <div id="menu-box-placeholder" style={this.state.placeholderColor}></div>
                 <ul
                     id="menu-ul">
                     <li className="animated bounceInLeft"
