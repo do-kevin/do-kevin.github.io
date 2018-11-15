633729fcfc987daadf0890dc597379b43cfe53fd
Kevin Do <do.kevin.vo@gmail.com>
2018-11-15 15:53:46 -0800

    Updates

--- precache-manifest.c51624871ac634d2f5fa46b9fb98adf6.js
+++ precache-manifest.c51624871ac634d2f5fa46b9fb98adf6.js
@@ -8,6 +8,10 @@ self.__precacheManifest = [
     "url": "/static/media/moodNmusic.3e46923d.png"
   },
   {
+    "revision": "561062e8004cc4b0a9fdb1773caff149",
+    "url": "/static/media/memorygame.561062e8.png"
+  },
+  {
     "revision": "48e545ea847b6b935203fd17349101ef",
     "url": "/static/media/flashme.48e545ea.png"
   },
@@ -16,19 +20,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "300f3a01bcebe25ab455",
-    "url": "/static/js/main.300f3a01.chunk.js"
+    "revision": "276f0bcbf4f134639808",
+    "url": "/static/js/main.276f0bcb.chunk.js"
   },
   {
-    "revision": "41571099ee3b01b4dcc1",
-    "url": "/static/js/1.41571099.chunk.js"
+    "revision": "c4169d2656405fbd4cae",
+    "url": "/static/js/1.c4169d26.chunk.js"
   },
   {
-    "revision": "300f3a01bcebe25ab455",
-    "url": "/static/css/main.352214cf.chunk.css"
+    "revision": "276f0bcbf4f134639808",
+    "url": "/static/css/main.216447a7.chunk.css"
   },
   {
-    "revision": "cf6099d981306496dcf2e80b7e36f763",
+    "revision": "648d74d9b0e96428c0ca69dc1c823205",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.011f1ee1ca386bf147d53f9a918b9dac.js"
+  "/precache-manifest.c51624871ac634d2f5fa46b9fb98adf6.js"
 );
 
 workbox.clientsClaim();
