1511745fe7cc0665cf2fb11d95c5bfe174d293ab
Kevin Do <do.kevin.vo@gmail.com>
2018-11-22 23:14:37 -0800

    Small edit to profile info

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-// import "../css/About.css";
-
 class About extends Component {
   render() {
     return (
@@ -11,12 +9,12 @@ class About extends Component {
             src={require("../static/images/about-picture.jpg")}
             alt="avatar"
             id="about-picture"
-            className="animated slideInUp delay-200ms"
+            className="animated pulse delay-200ms"
           />
         </figure>
-        <article className="section has-text-left animated slideInUp delay-200ms">
+        <article className="section has-text-left">
           <p id="profile-info">
-            I am a full-stack developer and an UCLAX coding bootcamp graduate
+            I am a front-end engineer and an UCLAX coding bootcamp graduate
             that is based in Los Angeles. Driven by curiosity, I strive to build
             a fun interactive experience between websites and users. I aim to
             hone my skills and to practice my craft so I can deliver functional
@@ -26,8 +24,8 @@ class About extends Component {
           <p id="profile-info" className="has-text-centered">
             View my resume or contact me:
             <br/><br/>
-            <a className="external-links button" href="https://www.visualcv.com/kevin-do-2018" target="_blank" rel="noreferrer noopener">
-              <i className="fas fa-portrait"></i>{" "}Resume
+            <a className="external-links button" href="https://www.visualcv.com/kevin-do-tech-18" target="_blank" rel="noreferrer noopener">
+              <i className="fas fa-portrait"></i>&nbsp;Resume
             </a>
             &nbsp;&nbsp;
             <a className="external-links button" href="https://www.linkedin.com/in/kevin-v-do" target="_blank" rel="noreferrer noopener">
