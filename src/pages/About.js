16ae083c80363d74f41ce2f689fc52fbcfa4e87f
Kevin Do <do.kevin.vo@gmail.com>
2018-12-16 00:55:37 -0800

    Re-styles Project One page and adds template info

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,13 +7,11 @@ class About extends Component {
         <nav className="custom-navbar">
           <nav className="custom-dropdown">
             <button 
-              className="drop-btn" 
-              style={{fontSize: "30px"}}>
+              className="drop-btn">
               Projects
             </button>
             <div className="drop-list">
               <button
-                style={{fontSize: "30px"}} 
                 onClick={this.props.handleProjectOne}>Mood & Music</button>
               <button>The Neverending Dead</button>
               <button>Zephyr Node</button>
--- src/pages/ProjectOne.js
+++ src/pages/ProjectOne.js
@@ -3,23 +3,20 @@ import React, { Component } from "react";
 class ProjectOne extends Component {
   render() {
     let fontColor = "#cacaca";
-
     return (
       <main
         style={{
           minHeight: "1200px",
-          background: "#2a2a2a"
+          background: "#00597b"
         }}>
         <nav className="custom-navbar">
           <nav className="custom-dropdown">
             <button 
-              className="drop-btn" 
-              style={{fontSize: "30px"}}>
+              className="drop-btn">
               Menu
             </button>
             <div className="drop-list">
-              <button 
-                style={{fontSize: "30px"}} 
+              <button
                 onClick={this.props.handleAbout}>About</button>
               <button>The Neverending Dead</button>
               <button>Zephyr Node</button>
@@ -39,20 +36,42 @@ class ProjectOne extends Component {
             Music & Mood
           </header> 
           <a 
-            className="button is-primary visit-website-btn"
+            className="visit-website-btn"
             href="https://do-kevin.github.io/Mood-and-Music" target="_blank" rel="noreferrer noopener">
-            VISIT THE WEBSITE</a>
+            <i className="fas fa-external-link-square-alt"></i>&nbsp;Live Website</a>
+          {"  "}
+          <a
+            className="visit-github-btn"
+            href="https://github.com/do-kevin/Mood-and-Music" target="_blank" rel="noreferrer noopener">
+            <i className="fab fa-github-alt"></i>&nbsp;GitHub</a>
           <a href="https://do-kevin.github.io/Mood-and-Music" target="_blank" rel="noreferrer noopener">
             <figure className="project-img-container">
                 <figure className="image is-16by9">
-                  <img src={require("../static/images/moodNmusic.png")} alt="moodNmusic.png"/>
+                  <img 
+                    style={{borderRadius: "5px"}}
+                    src={require("../static/images/moodNmusic.png")} alt="moodNmusic.png"/>
                 </figure>
             </figure>
           </a>
-          <h2 style={{fontSize: "24px", color: fontColor, marginTop: "2vh"}}>About this project</h2>
+          <h2 style={{fontSize: "24px", color: fontColor, marginTop: "2vh"}}>About the project</h2>
           <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
           <p style={{textAlign: "left", color: fontColor}}>
             On this project, I was responsible for implementing the webcam using WebRTC API. I've also worked on including Napter API as well and combining the two APIs together.
+          </p>
+          <h2 style={{fontSize: "24px", color: fontColor, marginTop: "2vh"}}>Role</h2>
+          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <p style={{textAlign: "left", color: fontColor}}>
+            I was the front-end developer.
+          </p>
+          <h2 style={{fontSize: "24px", color: fontColor, marginTop: "2vh"}}>Timeframe</h2>
+          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <p style={{textAlign: "left", color: fontColor}}>
+            Made in ten days
+          </p>
+          <h2 style={{fontSize: "24px", color: fontColor, marginTop: "2vh"}}>Technical Sheet</h2>
+          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <p style={{textAlign: "left", color: fontColor}}>
+            The coding technologies I use for this project are. . .
           </p>
         </section>
       </main>
