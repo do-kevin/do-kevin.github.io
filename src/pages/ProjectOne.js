e57b10da9a8e186845e2e4269249ae8da7effd0e
Kevin Do <do.kevin.vo@gmail.com>
2018-12-16 20:37:46 -0800

    Adds bg colors to elements

--- src/pages/ProjectOne.js
+++ src/pages/ProjectOne.js
@@ -55,34 +55,34 @@ class ProjectOne extends Component {
             </figure>
           </a>
           <h2 style={{fontSize: "24px", color: fontColor, marginTop: h2marginTop}}>About the project</h2>
-          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <hr style={{height: "1px", width: "200px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
           <p style={{textAlign: "left", color: fontColor}}>
             A front-end web application that analyzes an image file whether it was taken using the webcam feature or uploaded by the user. After it successfully detects a facial emotion, it will play back a song associated with that emotion.
             <br/><br/>
             On this project, I was the front-end developer responsible for implementing the webcam by using WebRTC API. Some of WebRTC API's code had been deprecated so I had to research for updated code. I also worked on implementing Napster API and converting Base64 to JPEG format. The project used Materialize as its CSS framework, which I used to visually tweak the project.
           </p>
           <h2 style={{fontSize: "24px", color: fontColor, marginTop: h2marginTop}}>Project Date</h2>
-          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <hr style={{height: "1px", width: "139px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
           <p style={{textAlign: "left", color: fontColor}}>
             Sept. 7th - Sept. 11th, 2018 
           </p>
           <h2 style={{fontSize: "24px", color: fontColor, marginTop: h2marginTop}}>Roadblocks</h2>
-          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <hr style={{height: "1px", width: "135px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
           <ul style={{listStyleType: "circle", color: "white", marginLeft: "20px"}}>
             <li>Could not use Napster SDK because it was outdated.</li>
             <li>Figuring out how to convert Base64 code into JPEG.</li>
           </ul> 
           <h2 style={{fontSize: "24px", color: fontColor, marginTop: h2marginTop}}>Technical Sheet</h2>
-          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <hr style={{height: "1px", width: "182px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
           <section className="technical-sheet">
-            <div className="technical-list-item">jQuery</div>
-            <div className="technical-list-item">jQuery UI</div>
-            <div className="technical-list-item">Firebase</div>
-            <div className="technical-list-item">Materialize CSS</div>
+            <div className="technical-list-item" style={{backgroundColor: "#0068af"}}>jQuery</div>
+            <div className="technical-list-item" style={{backgroundColor: "#b4491e"}}>jQuery UI</div>
+            <div className="technical-list-item" style={{backgroundColor: "#ffcb00", color: "#2c384b"}}>Firebase</div>
+            <div className="technical-list-item" style={{backgroundColor: "#f06d71"}}>Materialize CSS</div>
             <div className="technical-list-item">HTML</div>
           </section>
           <h2 style={{fontSize: "24px", color: fontColor, marginTop: h2marginTop}}>Teammates</h2>
-          <hr style={{height: "1px", width: "250px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
+          <hr style={{height: "1px", width: "132px", marginTop: "1vh", marginBottom: "1vh", background: fontColor}}/>
           <section className="teammate-list">
             <figure className="teammate-container">
               <a
