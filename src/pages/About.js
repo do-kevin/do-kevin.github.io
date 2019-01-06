28ae5bd6287cf8e899f9824a4ed31bf6274d78e0
Kevin Do <do.kevin.vo@gmail.com>
2019-01-05 19:10:40 -0800

    Adjusts negative space

--- src/pages/About.js
+++ src/pages/About.js
@@ -49,17 +49,17 @@ class About extends Component {
           <i className="fas fa-file-alt"></i>&nbsp;Resume <span>.docx</span></a>
           &nbsp;
         </section>
-                {/* About me */}
-                <header id="hi-header"
+        {/* About me */}
+        <header id="hi-header"
           style={{
             textAlign: "center", 
             color: "white"}}>
         Creative and experience-driven, I care deeply about creating beautiful user journeys and delivering performant code</header>
-        <div style={{minWidth: "400px", maxWidth: "720px", margin: "auto", marginBottom: "50px"}}>
+        <div style={{minWidth: "400px", maxWidth: "720px", margin: "auto", marginBottom: "45px"}}>
           <p id="about-info">
             Promotional websites that follow after film or video game trailers have always left me a lasting impression since high school. It wasn't until I earned my B.A. in English at UC Davis and working as a freelance copy editor that my curiosity and interest in web development catalyzed.
             <br/><br/>
-            I had copyedited and line edited for a firm's web site regarding information about its personal injury practices. While I examined the website, I began to deconstruct it by mapping its navigation, manually testing it for mobile responsiveness and errors. This experience led me to provide UI/UX suggestions on top of the edits for the web developers.
+            I had copyedited and line edited for a firm's website regarding information about its personal injury practices. While I examined the website, I began to deconstruct it by mapping its navigation, manually testing it for mobile responsiveness and errors. This experience led me to provide UI/UX suggestions on top of the edits for the web developers.
             <br/><br/>
             From there, I ventured to complete UCLA's full-stack coding bootcamp. Building websites with others under a short amount of time has given me a certain hunger. It was thrilling, creative, intense, and fun. I want to digest new technologies and implement them to build websites that will improve people's lives or provide a transcendent experience.
           </p>
