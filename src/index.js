6fcf9dcc3309111069a1fe2dee9fc86f5b66cef1
Kevin Do <do.kevin.vo@gmail.com>
2019-01-10 00:52:21 -0800

    Adds LogRocket

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LogRocket from 'logrocket';
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+LogRocket.init('d6pvgs/portfolio-github-pages');
 serviceWorker.unregister();
